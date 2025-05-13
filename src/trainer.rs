ea3945053cd2605a545276260a30422f4713be45
z2_ <88509734+z2-2z@users.noreply.github.com>
2025-05-13 23:39:16 +0200

    Interval buttons for all difficulty levels

--- src/trainer.rs
+++ src/trainer.rs
@@ -9,11 +9,28 @@ const ADVANCED_INTERVALS: [usize; 14] = [
     1,2,3,4,5,6,7,8,9,10,11,12,13,14
 ];
 
+fn interval_name(interval: usize) -> &'static str {
+    match interval {
+        1 => "Minor 2nd",
+        2 => "Major 2nd",
+        3 => "Minor 3rd",
+        4 => "Major 3rd",
+        5 => "Perfect 4th",
+        6 => "Tritone",
+        7 => "Perfect 5th",
+        8 => "Minor 6th",
+        9 => "Major 6th",
+        10 => "Minor 7th",
+        11 => "Major 7th",
+        12 => "Octave",
+        13 => "Minor 9th",
+        14 => "Major 9th",
+        _ => unreachable!(),
+    }
+}
+
 #[component]
 pub fn TrainerView() -> Element {
-    let audio_files: &[Asset] = match CONFIG().instrument {
-        Instrument::Piano => instrument::PIANO.as_ref(),
-    };
     let round = use_signal(|| 0usize);
     
     rsx! {
@@ -33,21 +50,25 @@ pub fn TrainerView() -> Element {
         
         IntervalGuesser {
             round: round,
-            audio_files,
         }
     }
 }
 
 #[component]
-fn IntervalGuesser(round: Signal<usize>, audio_files: &'static [Asset]) -> Element {
+fn IntervalGuesser(round: Signal<usize>) -> Element {
+    let audio_files: &[Asset] = match CONFIG().instrument {
+        Instrument::Piano => instrument::PIANO.as_ref(),
+    };
     let difficulty = CONFIG().difficulty;
+    let interval_list = match &difficulty {
+        Difficulty::Basic => BASIC_INTERVALS.as_ref(),
+        Difficulty::Advanced => ADVANCED_INTERVALS.as_ref(),
+    };
+    
     let mut rng = rand::rng();
     rng.reseed().expect("Could not seed RNG");
     let ascending = rng.random::<bool>();
-    let interval = match &difficulty {
-        Difficulty::Basic => BASIC_INTERVALS[rng.random_range(..BASIC_INTERVALS.len())],
-        Difficulty::Advanced => ADVANCED_INTERVALS[rng.random_range(..ADVANCED_INTERVALS.len())],
-    };
+    let interval = interval_list[rng.random_range(..interval_list.len())];
     let (first, second) = if ascending {
         let first = rng.random_range(..audio_files.len() - interval);
         (first, first + interval)
@@ -55,15 +76,12 @@ fn IntervalGuesser(round: Signal<usize>, audio_files: &'static [Asset]) -> Eleme
         let first = rng.random_range(interval..audio_files.len());
         (first, first - interval)
     };
+    
     let mut wrong = use_signal(|| false);
 
     rsx! {
         h1 {
             "Trainer"
-        }
-        
-        p {
-            "ascending={ascending} firs={first} second={second} interval={interval}"
         }
         
         audio {
@@ -97,7 +115,7 @@ fn IntervalGuesser(round: Signal<usize>, audio_files: &'static [Asset]) -> Eleme
                             second.pause();
                             first.currentTime = 0;
                             second.currentTime = 0;
-                        }, 2000);
+                        }, 2500);
                     "#
                 ).await.expect("Eval JS code failed");
             },
@@ -105,34 +123,32 @@ fn IntervalGuesser(round: Signal<usize>, audio_files: &'static [Asset]) -> Eleme
             "play"
         }
         
-        match &difficulty {
-            Difficulty::Basic => {
-                
-            },
-            Difficulty::Advanced => {},
-        }
-        
-        button {
-            onclick: move |_| *wrong.write() = true,
+        div {
+            id: "interval-selection",
             
-            "wrong"
-        }
-        
-        button {
-            onclick: move |_| {
-                let stats = &mut CONFIG.write().stats;
-                if !wrong() {
-                    stats.streak += 1;
-                    stats.right[interval - 1] += 1;
-                } else {
-                    stats.streak = 0;
-                    stats.wrong[interval - 1] += 1;
+            for i in interval_list {
+                button {
+                    key: "interval-{i}",
+                    onclick: move |_| {
+                        if *i == interval {
+                            let stats = &mut CONFIG.write().stats;
+                            if !wrong() {
+                                stats.streak += 1;
+                                stats.right[interval - 1] += 1;
+                            } else {
+                                stats.streak = 0;
+                                stats.wrong[interval - 1] += 1;
+                            }
+                            *round.write() += 1;
+                            *wrong.write() = false;
+                        } else {
+                            *wrong.write() = true;
+                        }
+                    },
+                    
+                    "{interval_name(*i)}"
                 }
-                *round.write() += 1;
-                *wrong.write() = false;
-            },
-            
-            "right"
+            }
         }
     }
 }
