00bc5d633f1846d6e424456863322f55b0cf7743
z2_ <88509734+z2-2z@users.noreply.github.com>
2025-05-16 22:28:11 +0200

    Started with stats

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,19 +1,98 @@
 use dioxus::prelude::*;
-use crate::{CONFIG, Route};
+use crate::{CONFIG, Route, Stats};
+
+#[inline(always)]
+fn stats_score(stats: &Stats, interval: usize) -> isize {
+    stats.right[interval] as isize - stats.wrong[interval] as isize
+}
+
+fn sort_by_score(stats: &Stats) -> Vec<usize> {
+    let mut ret = Vec::new();
+    
+    for i in 0..stats.wrong.len() {
+        if stats.right[i] != 0 && stats.wrong[i] != 0 {
+            ret.push(i);
+        }
+    }
+    
+    if !ret.is_empty() {
+        let mut modified;
+    
+        loop {
+            modified = false;
+            
+            for i in 0..ret.len() - 1 {
+                let first = stats_score(stats, ret[i]);
+                let second = stats_score(stats, ret[i + 1]);
+                
+                if second < first {
+                    ret.swap(i, i + 1);
+                    modified = true;
+                }
+            }
+            
+            if !modified {
+                break;
+            }
+        }
+    }
+    
+    ret
+}
 
 #[component]
 pub fn StatisticsView() -> Element {
     let stats = CONFIG().stats.clone();
+    let total_right = stats.right.iter().sum::<usize>();
+    let score = total_right as f64 * 100.0 / stats.total as f64;
+    let indices = sort_by_score(&stats);
     
     rsx! {
-        Link {
-            to: Route::TrainerView {},
+        div {
+            class: "tabs is-centered pt-2 is-size-6",
             
-            "back"
+            ul {
+                li {
+                    Link {
+                        to: Route::TrainerView {},
+                        "Trainer"
+                    }
+                }
+                li {
+                    class: "is-active",
+                    Link {
+                        to: "",
+                        "Statistics"
+                    }
+                }
+                li {
+                    Link {
+                        to: Route::SettingsView {},
+                        "Settings"
+                    }
+                }
+            }
         }
         
-        p {
-            "{stats:#?}"
+        if stats.total == 0 {
+            p {
+                class: "has-text-centered",
+                "no data yet"
+            }
+        } else {
+            h1 {
+                class: "subtitle is-4 has-text-centered",
+                
+                "Score: {score:.00}%"
+            }
+            
+            p {
+                "{stats:#?}"
+            }
+            
+            p {
+                "{indices:#?}"
+            }
         }
     }
 }
