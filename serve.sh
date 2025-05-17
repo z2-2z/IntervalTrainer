03a9239aa9746a57095e1fc2e059225559b7f5f9
z2_ <88509734+z2-2z@users.noreply.github.com>
2025-05-17 22:47:51 +0200

    Updated home directory

--- serve.sh
+++ serve.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-export ANDROID_HOME=/home/z2_/.android/AndroidSdk
-export ANDROID_NDK_HOME=/home/z2_/.android/AndroidSdk/ndk/29.0.13113456
+export ANDROID_HOME="$HOME/.android/AndroidSdk"
+export ANDROID_NDK_HOME="$HOME/.android/AndroidSdk/ndk/29.0.13113456"
 
 if [[ -z "$(archlinux-java status | grep default | grep java-24-openjdk)" ]];
 then
